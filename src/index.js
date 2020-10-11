b1fa0ab82e4217ebc8835919042291530e1f8fe9
negative1skunk@gmail.com <negative1skunk@gmail.com>
2020-10-11 17:34:44 -0500

    up to hour 2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDom from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// CSS
+import "./index.css";
+
+const BookList = () => {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+};
+
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/A1XyS0D1S-L._AC_UL200_SR200,200_.jpg"
+    alt=""
+  />
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Title = () => (
+  <h1 style={{ color: "#617d98", fontSize: "2rem", margin: "0.25rem" }}>Room on a Broom</h1>
+);
+
+const Author = () => <h4>Jane Doe</h4>;
+
+ReactDom.render(<BookList />, document.getElementById("root"));
